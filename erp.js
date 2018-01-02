f636de579e919fbc5b72714966376e281fe88c2d
Sergio Gomez <sergiogmz@hotmail.es>
2018-01-02 17:29:22 +0100

    Version 2.0.0

--- erp.js
+++ erp.js
@@ -1,3 +1,4 @@
+"use strict";
 
 function BaseException() {
 }
@@ -7,13 +8,15 @@ BaseException.prototype.toString = function(){
 	return this.name + ": " + this.message;
 };
 
+//Excepción general.
 function StoreHouseException() {
 	this.name = "StoreHouseException";
-	this.message = "Error. StoreHouse Exception.";
+	this.message = "Error. StoreHouse Generic Exception.";
 }
 StoreHouseException.prototype = new BaseException();
 StoreHouseException.prototype.constructor = StoreHouseException;
 
+//Excepción que se lanza cuando un parámetro no es un objeto Storehouse.
 function ObjectStoreHouseException() {
 	this.name = "ObjectStoreHouseException";
 	this.message = "Error. The method needs a object Storehouse.";
@@ -21,6 +24,7 @@ function ObjectStoreHouseException() {
 ObjectStoreHouseException.prototype = new BaseException();
 ObjectStoreHouseException.prototype.constructor = ObjectStoreHouseException;
 
+//Excepción que se lanza cuando un parámetro no es un objeto Category.
 function CategoryStoreHouseException() {
 	this.name = "CategoryStoreHouseException";
 	this.message = "Error. The method needs a object Category.";
@@ -28,7 +32,7 @@ function CategoryStoreHouseException() {
 CategoryStoreHouseException.prototype = new BaseException();
 CategoryStoreHouseException.prototype.constructor = CategoryStoreHouseException;
 
-
+//Excepción que se lanza cuando un objeto category ya existe.
 function CategoryExistsException() {
 	this.name = "CategoryExistsException";
 	this.message = "Error. The category already exist.";
@@ -36,6 +40,7 @@ function CategoryExistsException() {
 CategoryExistsException.prototype = new BaseException();
 CategoryExistsException.prototype.constructor = CategoryExistsException;
 
+//Excepción que se lanza cuando un objeto category no existe.
 function CategoryNoExistsException() {
 	this.name = "CategoryNoExistsException";
 	this.message = "Error. The category not exist.";
@@ -44,13 +49,35 @@ CategoryNoExistsException.prototype = new BaseException();
 CategoryNoExistsException.prototype.constructor = CategoryNoExistsException;
 
 
-
+//Excepción que se lanza cuando un objeto category ya existe.
 function ParameterValidationException() {
 	this.name = "ParameterValidationException";
 	this.message = "Error. Parameter Validation Exception.";
 }
 ParameterValidationException.prototype = new BaseException();
 ParameterValidationException.prototype.constructor = ParameterValidationException;
+
+//Excepción que se lanza cuando un parámetro no es un objeto Product.
+function ProductStoreHouseException() {
+	this.name = "ProductStoreHouseException";
+	this.message = "Error. The method needs a object Product.";
+}
+ProductStoreHouseException.prototype = new BaseException();
+ProductStoreHouseException.prototype.constructor = ProductStoreHouseException;
+
+function ProductCategoryExistsException(product) {
+	this.name = "ProductCategoryExistsException";
+	this.message = "Error. "+product+" already exist in the category.";
+}
+ProductCategoryExistsException.prototype = new BaseException();
+ProductCategoryExistsException.prototype.constructor = ProductCategoryExistsException;
+
+function ShopStoreHouseException() {
+	this.name = "ShopStoreHouseException";
+	this.message = "Error. The method needs a object Product.";
+}
+ShopStoreHouseException.prototype = new BaseException();
+ShopStoreHouseException.prototype.constructor = ShopStoreHouseException;
 
 function EmptyValueException(param) {
 	this.name = "EmptyValueException";
@@ -69,7 +96,7 @@ InvalidAccessConstructorException.prototype.constructor = InvalidAccessConstruct
 
 
 function StoreHouse(){
-	//La función se invoca con el operador new
+
 	if (!(this instanceof StoreHouse)) 
 		throw new InvalidAccessConstructorException();
 
@@ -80,6 +107,7 @@ function StoreHouse(){
     var shops = [];
     var stock = [];
     
+    //Definición de los metodos 'get' y 'set' del atributo name
     Object.defineProperty(this, 'name', {
 			get:function(){
 				return name;
@@ -91,6 +119,7 @@ function StoreHouse(){
 			}		
 		});
 	
+    //Definición del metodo 'get' del atributo categories. Devuelve iterador
     Object.defineProperty(this, 'getCategories', {
 		get:function(){
             var nextIndex = 0;
@@ -102,6 +131,7 @@ function StoreHouse(){
 		}	
 	});
     
+    //Definición del metodo 'get' del atributo sjops. Devuelve iterador
     Object.defineProperty(this, 'getShops', {
 		get:function(){
             var nextIndex = 0;
@@ -115,17 +145,16 @@ function StoreHouse(){
     
     
     this.addCategory = function(category){
-		if (!(category instanceof Category)) { 
-			throw new CategoryStoreHouseException ();
+		if (!(category instanceof Category)){ 
+			throw new CategoryStoreHouseException();
 		}
         
         if (category == null){ 
 			throw new EmptyValueException(category);
 		}
         
-		var index = getCategoryIndex(category);
-        
-        if (index === -1){
+		var indexCategory = getCategoryIndex(category);
+        if (indexCategory === -1){
             categories.push(category);
         }else{
             throw new CategoryExistsException();
@@ -139,10 +168,9 @@ function StoreHouse(){
 			throw new CategoryStoreHouseException();
 		}
         
-		var index = getCategoryIndex(category);
-        
-        if (index !== -1){
-            categories.splice(index, 1);
+		var indexCategory = getCategoryIndex(category);   
+        if (indexCategory !== -1){
+            categories.splice(indexCategory, 1);
         }else{
             throw new CategoryNoExistsException();
         }
@@ -156,7 +184,7 @@ function StoreHouse(){
         }		
 
         function compareElements(element) {
-            return (element.name === category.name)
+            return (element.title === category.title)
         }
 				
         return category.findIndex(compareElements);		
@@ -176,7 +204,7 @@ function StoreHouse(){
     
     this.addProduct = function(product, category){
 		if (!(product instanceof Product)) { 
-			throw new ImageImageManagerException();
+			throw new ProductStoreHouseException();
 		}	
 		if (product === null){
 			throw new EmptyValueException(product);
@@ -191,7 +219,6 @@ function StoreHouse(){
 			products.push(product);
 		}
         
-        
         var categoryPosition = getCategoryIndex(category); 
 		if (categoryPosition === -1){
             categoryPosition = this.addCategory(category)-1;
@@ -201,7 +228,7 @@ function StoreHouse(){
 		if (productCategoryPos === -1){
 			categories[categoryPosition].products.push(product);
 		}else{
-            throw new ImageExistsImageManagerException(category);
+            throw new ProductCategoryExistsException(product);
 		}	
         return categories[categoryPosition].products.length;
 	}
@@ -222,6 +249,128 @@ function StoreHouse(){
                     }
                 }
 	}
+    
+    /*this.removeProduct = function(product){
+				if (!(product instanceof Product)) { 
+					throw new ImageImageManagerException();
+				}				
+
+				var i = categories.length - 1, position = -1;
+				while (i >= 0 && position === -1){					
+					position = getCategoryProducts(product, categories[i].products); 
+					i--;
+				}		
+
+				if (position !== -1){
+					categories[i+1].products.splice(position, 1);
+				} else {
+					throw new ImageNotExistsImageManagerException();
+				}
+			}*/
+    this.addProductInShop = function(product, shop, num){
+				if (!(product instanceof Product)) { 
+					throw new ProductStoreHouseException();
+				}	
+
+				if (!(shop instanceof Shop)) { 
+					throw new ShopStoreHouseException();
+				}		
+				if (shop === null || shop === 'undefined' || shop === ''){
+					shop = this.defaultShop;
+				}	
+
+				//Obtenemos posición de la categoría. Si no existe se añade.
+				var productPosition = getProductIndex(product); 
+				if (productPosition === -1){
+					products.push(product);
+				}	
+
+				//Obtenemos posición del autor. Si no existe se añade.
+				var shopPosition = getShopPosition(shop); 
+				if (shopPosition === -1){
+					shopPosition = this.addShop(shop)-1;
+				}
+
+				//Obtenemos posición de la imagen en la categoría. Si no existe se añade. Si existe se lanza excepción.
+				var productShopPosition = getShopProductPosition(product, shops[shopPosition].products); 	
+				if (productShopPosition === -1){
+					shops[shopPosition].products.push(
+						{
+							stock: num
+						}
+					);
+				}else{
+					throw new ImageExistsImageManagerException(category);
+				}	
+
+				return shops[shopPosition].products.length;
+			}
+    
+   this.addQuantityProductInShop = function(product, shop, num){
+				if (!(product instanceof Product)) { 
+					throw new ProductStoreHouseException();
+				}	
+
+				if (!(shop instanceof Shop)) { 
+					throw new ShopStoreHouseException();
+				}		
+
+				var productPosition = getProductIndex(product); 
+				if (productPosition === -1){
+					//throw new 
+				}	
+
+				var shopPosition = getShopPosition(shop); 
+				if (shopPosition === -1){
+					//throw new 
+				}
+
+				//Obtenemos posición de la imagen en la categoría. Si no existe se añade. Si existe se lanza excepción.
+				var productShopPosition = getShopProductPosition(product, shops[shopPosition].products); 	
+				if (productShopPosition !== -1){
+					shops[shopPosition].products[productShopPosition].stock += num;
+				}else{
+					throw new ImageExistsImageManagerException(category);
+				}	
+
+				return shops[shopPosition].products.length;
+			}
+                    
+    this.addShop = function(shop){
+		if (!(shop instanceof Shop)){ 
+			throw new ShopStoreHouseException();
+		}
+        
+        if (shop == null){ 
+			throw new EmptyValueException(category);
+		}
+        
+		var indexShop = getShopIndex(shop);
+        if (indexShop === -1){
+            shops.push(shop);
+        }else{
+            throw new ShopExistsException();
+        }
+        
+        return shops.length();
+	}
+    
+    this.removeShop = function(shop){
+		if (!(shop instanceof Shop)) { 
+			throw new ShopStoreHouseException();
+		}
+        
+		var indexShop = getShopIndex(category);   
+        if (indexShop !== -1){
+            shops.splice(indexShop, 1);
+        }else{
+            throw new ShopNoExistsException();
+        }
+        
+        return shops.length();
+	}                
+    
+    
 }
 
 
